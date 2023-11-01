f8fac30666590575ee797bc978c377067a594a24
Quinn Peterson <qap2001@gmail.com>
2023-11-01 00:28:25 -0700

    Proj3: current state

--- Proj3/src/main.py
+++ Proj3/src/main.py
@@ -5,14 +5,49 @@ NUM_FRAMES = 256
 PF_SIZE = 256 
 TLB_SIZE = 16 
 PRA = 'fifo'
+NUM_FAULTS = 0 
 
 class PageTable:
     def __init__(self):
-        self.entries = [{'f': None, 'loaded': False} for _ in range(NUM_PAGES)]
+        # pair represents: (frame number, loaded bit)
+        # index of entries represents the page number 
+        self.entries = [(-1, False) for _ in range(NUM_PAGES)]
+
+    def idx(self, pageNumber):
+        return self.entries[pageNumber] if self.entries[pageNumber] is not None else -1
+
 
 class TLB: 
+    # TODO refactor so stuff is put and taken out in FIFO order 
     def __init__(self):
-        self.entries = [{'p': None, 'f': None} for _ in range(TLB_SIZE)]
+        # pair represents: (page number, frame number)
+        self.entries = [(None, None) for _ in range(TLB_SIZE)]
+
+    def idx(self, pageNumber):
+        for entry in self.entries:
+            if entry[0] == pageNumber:
+                return entry[1]
+        return -1
+    
+class PhysicalMemory:
+    def __init__(self, numFrames):
+        self.frames = [{'content': None, 'pageNumber': None} for _ in range(numFrames)]
+        self.numFrames = numFrames
+
+    def load_page(self, pageNumber, content):
+        for frame in self.frames:
+            if frame['content'] is None:
+                frame['content'] = content
+                frame['pageNumber'] = pageNumber
+                return True
+        return False
+    
+    # TODO refactor; might need to use a free-frames queue instead 
+    def find_free(self):
+        for index, frame in enumerate(self.frames):
+            if frame['content'] is None and frame['pageNumber'] is None:
+                return index
+        return -1 
 
 def split_virtual(addr):
     if not 0 <= addr <= 65535:
@@ -20,10 +55,10 @@ def split_virtual(addr):
     
     binary_representation = format(addr, '016b')
     
-    page_number = int(binary_representation[:8], 2)
-    frame_offset = int(binary_representation[-8:], 2)
+    pageNumber = int(binary_representation[:8], 2)
+    frameOffset = int(binary_representation[-8:], 2)
     
-    return page_number, frame_offset
+    return pageNumber, frameOffset
 
 
 
@@ -34,7 +69,7 @@ def main():
         print('Error: Must provide at least one argument')
         sys.exit(1)
 
-    global NUM_FRAMES, PRA
+    global NUM_FRAMES, PRA, NUM_FAULTS
 
     rf = sys.argv[1]
     
@@ -50,12 +85,47 @@ def main():
 
     print('got inputs: \n\t file: %s \n\t frames: %d \n\t PRA %s' % (rf, NUM_FRAMES, PRA))
 
+    # ---- instantiate main hardware / mmu structures 
+    pageTable = PageTable()
+    tlb = TLB()
+    physMem = PhysicalMemory(NUM_FRAMES)
+
     # ----- 
     try:
         with open(rf, 'r') as file:
             for line in file:
-                num = int(line.strip())
-                print(num) 
+                virtualAddr = int(line.strip())
+                
+                pageNumber, frameOffset = split_virtual(virtualAddr)
+
+                frameNumber = -1 
+
+                # check TLB
+                frameNumber = tlb.idx(pageNumber)
+                if frameNumber == -1: 
+                    # check page table 
+                    frameNumber, loadedBit = pageTable.idx(pageNumber)
+                    if frameNumber == -1: 
+
+                        # --- PAGE FAULT: case where page entry not found in TLB nor page table 
+                        # bring in missing page (TODO for now assuming no swapping required)
+                        freeIdx = physMem.find_free()
+                        if freeIdx == -1:
+                            # TODO swapping 
+                            pass 
+
+
+
+                        # correct page table and other structures 
+
+
+
+
+
+
+
+                # testing this for one addr for now 
+                return 
     except FileNotFoundError:
         print(f'Error: The file {rf} was not found')
         sys.exit(1)
