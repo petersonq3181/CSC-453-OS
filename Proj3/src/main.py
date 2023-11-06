882049ac2127f583ce669ba5983bd3339ca56851
Quinn Peterson <qap2001@gmail.com>
2023-11-05 17:02:46 -0800

    Proj3: pra LRU

--- Proj3/src/main.py
+++ Proj3/src/main.py
@@ -66,7 +66,8 @@ class PhysicalMemory:
     def __init__(self, numFrames):
         self.frames = [{'content': None, 'pageNumber': None} for _ in range(numFrames)]
         self.numFrames = numFrames
-        self.pageOrderQueue = collections.deque()  # FIFO queue to store the order of loaded pages
+        self.pageOrderQueue = collections.deque()
+        self.lruCache = collections.OrderedDict()
 
     # original load page (TODO might delete later)
     def load_page(self, pageNumber, content):
@@ -84,7 +85,7 @@ class PhysicalMemory:
         if freeFrameIndex != -1:
             self.frames[freeFrameIndex]['content'] = content
             self.frames[freeFrameIndex]['pageNumber'] = pageNumber
-            self.pageOrderQueue.append((pageNumber, freeFrameIndex))  # Add the page to the FIFO queue
+            self.pageOrderQueue.append((pageNumber, freeFrameIndex))
             pageTable.update_entry(pageNumber, freeFrameIndex, True)
             return freeFrameIndex
         
@@ -100,11 +101,48 @@ class PhysicalMemory:
         tlb.remove_entry(oldPageNumber)
         return oldFrameIndex
     
+    def load_page_lru(self, pageNumber, content, pageTable, tlb):
+        # if page is already loaded
+        if pageNumber in self.lruCache:
+            frameIndex = self.lruCache.pop(pageNumber)
+            self.frames[frameIndex]['content'] = content
+            self.lruCache[pageNumber] = frameIndex
+            return frameIndex
+        
+        freeFrameIndex = self.find_free()
+
+        # if there is a free frame
+        if freeFrameIndex != -1:
+            self.frames[freeFrameIndex]['content'] = content
+            self.frames[freeFrameIndex]['pageNumber'] = pageNumber
+            self.lruCache[pageNumber] = freeFrameIndex
+            pageTable.update_entry(pageNumber, freeFrameIndex, True)
+            return freeFrameIndex
+
+        # if no free frame is available evict the LRU page
+        lruPageNumber, lruFrameIndex = self.lruCache.popitem(last=False)
+        self.frames[lruFrameIndex]['content'] = content
+        self.frames[lruFrameIndex]['pageNumber'] = pageNumber
+        self.lruCache[pageNumber] = lruFrameIndex
+
+        # update page table and TLB for the evicted page
+        pageTable.update_entry(lruPageNumber, -1, False)
+        tlb.remove_entry(lruPageNumber)
+        # update page table and TLB for the new page
+        pageTable.update_entry(pageNumber, lruFrameIndex, True)
+
+        return lruFrameIndex
+    
     def find_free(self):
         for index, frame in enumerate(self.frames):
             if frame['content'] is None and frame['pageNumber'] is None:
                 return index
         return -1 
+    
+    def record_access(self, pageNumber):
+        if pageNumber in self.lruCache:
+            frameIndex = self.lruCache.pop(pageNumber)
+            self.lruCache[pageNumber] = frameIndex
     
     def print_state(self): 
         print('\n----- Physical Memory:')
@@ -184,7 +222,7 @@ def main():
         try:
             NUM_FRAMES = int(sys.argv[2])
 
-            if not (0 <= NUM_FRAMES < PF_SIZE):
+            if not (0 < NUM_FRAMES <= PF_SIZE):
                 raise ValueError
         except ValueError:
             print('Error: NUM_FRAMES must be an integer in a valid range')
@@ -234,9 +272,12 @@ def main():
                 if PRA.lower() == 'fifo': 
                     frameNumber = physMem.load_page_fifo(pageNumber, page, pageTable, tlb)
                 elif PRA.lower() == 'lru': 
-                    pass 
+                    frameNumber = physMem.load_page_lru(pageNumber, page, pageTable, tlb)
 
             tlb.add_entry(pageNumber, frameNumber)
+
+        if PRA.lower() == 'lru': 
+            physMem.record_access(pageNumber)
 
         print('{}, {}, {}, {}'.format(virtualAddr, value, frameNumber, format_byte_arr(page)))
     print('Number of Translated Addresses = {}'.format(n))
